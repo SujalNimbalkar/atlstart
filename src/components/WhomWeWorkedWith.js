ceb583f6b48489ac2d4a69076a379d135d38b9d9
Sujal Satyawan Nimbalkar <97455752+Sujal2709@users.noreply.github.com>
2024-08-10 16:31:27 +0530

    infinte slider fixed

--- src/components/WhomWeWorkedWith.js
+++ src/components/WhomWeWorkedWith.js
@@ -13,7 +13,7 @@ const Marquee = () => {
   const upperMarquee = [logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8];
 
   return (
-    <section className="bg-white py-16 font-syne">
+    <section className="bg-white py-16 font-syne overflow-x-hidden">
       <div className="max-w-7xl mx-auto px-6 lg:px-8 text-center">
         <h2 className="text-4xl font-bold mb-8">Whom we have worked with?</h2>
         <div className="container mx-auto ">
